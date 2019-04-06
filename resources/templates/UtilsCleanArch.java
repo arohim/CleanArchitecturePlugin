e37c4bd1285d3a6bf3fc5f09da7808e93a58833e
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-04-06 17:44:56 +0700

    added git ignore file

--- resources/templates/UtilsCleanArch.java
+++
@@ -1,8 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-
-#parse("File Header.java")
-public class ${NAME} {
-
-    public static final String ENDPOINT = ""; // TODO: Add your endpoint server to use in retrofit
-
-}
